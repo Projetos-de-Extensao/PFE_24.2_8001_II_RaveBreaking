7b9e46d4dba3d4cc5b4bcbde33a6f9ccf23a812f
Viktor <viktormb2005@gmail.com>
2024-10-20 14:05:17 -0300

    i

--- site-prototipo/site/src/components/Hero.jsx
+++ site-prototipo/site/src/components/Hero.jsx
@@ -1,19 +1,14 @@
 function Hero() {
   return (
-    <section className="hero">
-          <div className="background-image">
-            <h1 className="hero-content">
-              Bem-vindo ao LollaPalooza!!
-              <br />
-              Uma experiência única com os maiores artistas do mundo em um festival inesquecível.
-              <br />
-              <p>Quer descobrir mais sobre o festival?</p>
-              <a href="/About" className="cta-button">Clique aqui</a>
-            </h1>
-            </div>
+    <section>
+      <div className="hero-content">
+        <h1>Bem-vindo ao LollaPalooza!!</h1>
+        <p>Quer descobrir mais sobre o festival?</p>
+        <a href="/About" className="cta-button">Clique aqui</a>
+      </div>
     </section>
-
   );
 }
 
 export default Hero;
+
