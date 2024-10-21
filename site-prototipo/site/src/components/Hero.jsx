66a2521de6265b0d482a2b78f1df0620c35854fb
sarahferrarir <sarahferrarir@gmail.com>
2024-10-21 16:32:22 -0300

    add img de fundo

--- site-prototipo/site/src/components/Hero.jsx
+++ site-prototipo/site/src/components/Hero.jsx
@@ -1,6 +1,9 @@
+import backgroundImg from '../images/fundo-home.jpg'
+
 function Hero() {
   return (
     <section>
+      <img src={backgroundImg} alt="foto de fundo" className="hero-image"/>
       <div className="hero-content">
         <h1>Bem-vindo ao LollaPalooza!!</h1>
         <p>Quer descobrir mais sobre o festival?</p>
