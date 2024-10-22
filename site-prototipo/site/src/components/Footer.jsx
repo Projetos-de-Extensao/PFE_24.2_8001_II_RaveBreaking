02222ef1b0bcc920f0cd8ed051715ce075903517
sarah ferrari <sarahferrarir@gmail.com>
2024-10-22 15:27:41 -0300

    finalizando o footer

--- site-prototipo/site/src/components/Footer.jsx
+++ site-prototipo/site/src/components/Footer.jsx
@@ -1,13 +1,32 @@
+import tiktokIcon from '../images/tiktok.png'
+import facebookIcon from '../images/facebook.png'
+import instaIcon from '../images/insta.png'
+import twitterIcon from '../images/twitter.png'
+import youtubeIcon from '../images/youtube.png'
+
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="footer-container">
+        <div className="footer-site">
+          <a href="https://www.lollapaloozabr.com/">lollapaloozabr.com</a>
+        </div>
         <div className="footer-social">
-          <a href="https://www.youtube.com"><i className="fa fa-youtube"></i></a>
-          <a href="https://www.instagram.com"><i className="fa fa-instagram"></i></a>
-          <a href="https://www.facebook.com"><i className="fa fa-facebook"></i></a>
-          <a href="https://www.twitter.com"><i className="fa fa-twitter"></i></a>
-          <a href="https://www.tiktok.com"><i className="fa fa-tiktok"></i></a>
+          <a href="https://www.tiktok.com/@lollapaloozabr?">
+          <img src={tiktokIcon} alt="icone do tiktok" className='footer-icons'/>
+          </a>
+          <a href="https://www.instagram.com/lollapaloozabr/">
+          <img src={instaIcon} alt="icone do instagram" className='footer-icons'/>
+          </a>
+          <a href="https://x.com/lollapaloozabr">
+          <img src={twitterIcon} alt="icone do twitter" className='footer-icons'/>
+          </a>
+          <a href="https://www.youtube.com/lollapaloozabr">
+          <img src={youtubeIcon} alt="icone do youtube" className='footer-icons'/>
+          </a>
+          <a href="https://www.facebook.com/LollapaloozaBR">
+          <img src={facebookIcon} alt="icone do facebook" className='footer-icons'/>
+          </a>
         </div>
       </div>
     </footer>
