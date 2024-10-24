5b7321da86aa1196127c0ba37b8d767f64b7467d
Ferrett27 <161031706+Ferrett27@users.noreply.github.com>
2024-10-23 23:44:37 -0300

    Restaurante (beta)

--- site-prototipo/site/src/components/Restaurantes.jsx
+++ site-prototipo/site/src/components/Restaurantes.jsx
@@ -1,10 +1,22 @@
+import choripane1 from '../images/choripane1.jpg'
 function Restaurantes(){
     return (
+        <>
         <div className="topo-res">
         <h1 className="h1-res">
             Confira as opções de restaurantes presentes no festival
         </h1>
         </div>
+        <section className="opcoes">
+        <div>
+            <h2 className="res-nome1">Chimi</h2>
+            <div className='Chimi'>
+                <h2 className='chimi-op1'>choripán pimentão</h2>
+                <img src={choripane1} alt='Foto do sanduiche choripane do restaurante' className='img-choripane' width="178" height="160" />
+            </div>
+        </div>
+        </section>
+        </>
     )
 }
 
