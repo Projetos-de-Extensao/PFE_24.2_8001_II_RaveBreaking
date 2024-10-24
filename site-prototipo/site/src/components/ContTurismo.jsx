a33df532e355ed700fc14453bb44781023d99bd7
viktor <viktormb2005@gmail.com>
2024-10-24 17:31:22 -0300

    g

--- site-prototipo/site/src/components/ContTurismo.jsx
+++ site-prototipo/site/src/components/ContTurismo.jsx
@@ -1,12 +1,10 @@
 import turismo1 from '../images/Turismo1.png'
+import turismo2 from '../images/Turismo2.png'
 
 function ContTurismo() {
     return (
-        <section className='flex-container-turismo'>
-            <div className='img-turismo-one'>
-                <img src={turismo1} alt='imagem 1 da página turismo' className='img-turismo-one' />
-            </div>
             <div>
+                <img src={turismo1} alt='imagem 1 da página turismo'/>
                 <h1 className='turismo-titulo'>O que fazer em São Paulo?</h1>
                 <p className="turismo-paragraph">
                 Se você está planejando ficar mais tempo em São Paulo e quer explorar a cidade além do Lollapalooza, há uma 
@@ -15,8 +13,15 @@ function ContTurismo() {
                 a cidade é um verdadeiro caldeirão de diversidade. Confira alguns dos principais pontos turísticos que você não pode perder e aproveite para 
                 conhecer o lado único e cosmopolita de São Paulo!
                 </p>
-            </div>
-        </section>
+                <img src={turismo2} alt= 'imagem 2 da página turismo'/>
+                <h2 className='turismo-titulo'>Avenida Paulista</h2>
+                <p className='turismo-paragraph'>Uma das avenidas mais icônicas do Brasil, a Avenida Paulista é um verdadeiro centro cultural e financeiro de São Paulo. 
+                    Aos domingos, a avenida fica fechada para carros e se transforma em um grande calçadão, onde artistas de rua, feiras, shows 
+                    e eventos culturais acontecem. 
+                    Não deixe de visitar o MASP (Museu de Arte de São Paulo) e explorar as várias galerias, livrarias e cafeterias ao longo do caminho.
+                </p>
+             </div>
+
 
     )
 }
