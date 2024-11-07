2c869adbdcb3518322668d78356c481db7cadf94
viktor <viktormb2005@gmail.com>
2024-11-07 16:53:05 -0300

    a

--- site-prototipo/site/src/components/MapaEvento.jsx
+++ site-prototipo/site/src/components/MapaEvento.jsx
@@ -1,8 +1,22 @@
 import mapa from '../images/mapa-festival.png'
+import legenda from '../images/legenda-festival.png'
 
-function MapaEvento(){
+function MapaEvento() {
   return (
-      <img src={mapa} alt="mapa dp festival" className="mapaImg"/>
+        <div className='local-div'>
+          <div>
+            <h1 className='legenda-mapa-fest'>Mapa do festival</h1>
+          </div>
+          <div>
+            <img src={mapa} alt="mapa dp festival" className="mapaImg"/>
+          </div>
+          <div>
+            <h2 className='legenda-mapa-fest'>Legenda ampliada:</h2>
+          </div>
+          <div>
+            <img src={legenda} alt="legenda ampliada do festival" className="mapaLeg"/>
+          </div>
+        </div>
   )
 }
 
